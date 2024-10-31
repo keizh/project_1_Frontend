9f1680a2a3b39d2ff5a71179191f646a4cd4709e
Keizh <krishnamulay05@gmail.com>
2024-10-31 14:59:35 +0530

    search box added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../features/Product/ProductSlice";
 import ProductHomeCart from "../components/ProductHomeCart";
 import { fetchByCategory } from "../features/Product/ProductSlice";
+import { Input } from "@material-tailwind/react";
 
 export default function Home() {
   const location = useLocation();
@@ -16,6 +17,7 @@ export default function Home() {
     (state) => state.products
   );
   const [productsState, setProductsState] = useState(null);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const authAccess = auth();
@@ -28,27 +30,59 @@ export default function Home() {
     }
   }, [navigate, dispatch, category]);
 
-  useEffect(() => {
-    setProductsState(products);
-  }, [products]);
+  // useEffect(() => {
+  //   setProductsState(products);
+  // }, [products]);
   //  && status == "loading"
   // && status == "successful"
+
+  const handleSearch = (e) => setSearch(e.target.value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (search != "") {
+        console.log(search);
+        console.log(productsState);
+        setProductsState((productsState) =>
+          productsState.filter((product) =>
+            new RegExp(search, "i").test(product.name)
+          )
+        );
+      } else {
+        setProductsState(products);
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [search, products]);
+
   return (
     <>
-      {products.length == 0 && <Loader />}
+      {products.length == 0 && status == "loading" && <Loader />}
       {products.length > 0 && (
         <div className="max-w-screen-xl mx-auto mt-[80px] mb-4 ">
-          <div className="flex justify-between">
+          <div className="flex flex-col  justify-center  gap-[10px]">
             <h1 className="px-3 max-w-screen-xl mx-auto mt-[20px] font-mono text-[40px] sm:text-[50px]">
               Products for {category}
             </h1>
-            <div></div>
+            <div className="w-96  mx-auto">
+              <Input
+                onChange={handleSearch}
+                label="search Products"
+                className=" mx"
+              />
+            </div>
           </div>
           <div className="px-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px]">
             {productsState &&
-              products.map((product, index) => (
+              productsState.map((product, index) => (
                 <ProductHomeCart key={index} product={product} />
               ))}
+            {productsState && productsState.length == 0 && (
+              <p className="mt-5 text-center">No Such Products Found</p>
+            )}
           </div>
         </div>
       )}
--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,7 +1,7 @@
 export default function fn() {
   return (
     <div className="h-[100vw] w-[100vw] flex item-center justify-center">
-      <h1>Loading</h1>
+      <h1>Loading.....</h1>
     </div>
   );
 }
